d9219d10d1e7d01b628324e5c8a9f6f9ef8ffd70
Local <beiyuan@me.com>
2022-12-03 00:31:40 +0800

    Initial commit

---
+++ src/main/java/com/beiyuan/CateSharingApplication.java
@@ -0,0 +1,13 @@
+package com.beiyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CateSharingApplication.class, args);
+    }
+
+}
