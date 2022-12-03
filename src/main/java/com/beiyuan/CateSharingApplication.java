17d6da0477c05679f2c5a8d245e56f0a80d621d7
Local <beiyuan@me.com>
2022-12-03 08:25:22 +0800

    first commit

--- src/main/java/com/beiyuan/CateSharingApplication.java
+++ src/main/java/com/beiyuan/CateSharingApplication.java
@@ -9,6 +9,7 @@ public class CateSharingApplication {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("while");
+        System.out.println("wel");
         SpringApplication.run(CateSharingApplication.class, args);
     }
 
